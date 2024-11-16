45da3825489c8263c92d46f7e60c0dea39fc59a7
Crosson David <crosson.david@gmail.com>
2024-11-17 00:22:18 +0100

    Prepare nix flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    utils.url = "github:numtide/flake-utils";
+    sbt.url = "github:zaninime/sbt-derivation";
+    sbt.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, utils, sbt}:
+  utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.default = pkgs.mkShell {
+      buildInputs = [pkgs.sbt pkgs.metals pkgs.hello];
+    };
+
+    packages.default = sbt.mkSbtDerivation.${system} {
+      pname = "nix-web-echo";
+      version = "1.0.0";
+      depsSha256 = "";
+
+      src = ./.;
+
+      buildInputs = [pkgs.sbt pkgs.jdk22_headless pkgs.makeWrapper];
+
+      buildPhase = "sbt Universal/packageZipTarball";
+
+      installPhase = ''
+          tar xf target/universal/web-echo.tgz --directory $out
+          wrapProgram $out/bin/web-echo \
+            --set PATH ${pkgs.lib.makeBinPath [ pkgs.gnused pkgs.coreutils pkgs.jdk22_headless pkgs.bash ]}
+      '';
+    };
+  });
+}
