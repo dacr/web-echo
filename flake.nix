9c369c8353d03e0a59a996235e3345a5cfd185d9
crosson david lanfeust <crosson.david@gmail.com>
2025-06-23 18:15:49 +0200

    Upgrade nixpkgs to nixos-25.05 in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     utils.url = "github:numtide/flake-utils";
     sbt.url = "github:zaninime/sbt-derivation";
     sbt.inputs.nixpkgs.follows = "nixpkgs";
